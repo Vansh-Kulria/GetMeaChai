6011fda94e350f70f98b75505381d886e1ed74a2
Vansh Kulria <vanshkulria05@gmail.com>
2025-06-23 00:16:47 +0530

    first commit

---
+++ Componants/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className=" bg-gray-600/30 text-white text-center py-4 mt-8">
+       <p>© 2025 Get me a Chai. All rights reserved.❤️</p>
+    </div>
+  )
+}
+
+export default Footer
---
+++ Componants/Navbar.js
@@ -0,0 +1,32 @@
+import Link from 'next/link'
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <nav className="bg-gray-600/30 text-white py-3 px-3 flex justify-between items-center">
+
+            <div className="logo">
+                <Link href="/" className="text-2xl font-bold">
+                    Get me a Chai
+                </Link>
+            </div>
+
+            <div>
+                <Link href="/login">
+                <button
+                
+                    type="button"
+                    className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl
+                    focus:ring-2 focus:outline-none focus:ring-gray-500/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 cursor-pointer
+                    transition-all duration-300"
+                >
+                    Log In
+                </button>
+                </Link>
+            </div>
+
+        </nav>
+    )
+}
+
+export default Navbar
